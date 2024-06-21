668744da1b7d4c962f33bd65db1222ab114477a3
danielvallejo237 <daniel.vallejo@cimat.mx>
2024-06-21 12:39:49 -0600

    toDownload

--- DataPreProcessing/example.sh
+++ DataPreProcessing/example.sh
@@ -1,9 +1,15 @@
 #!/bin/bash
 USERNAME="viking"
-echo "Downloading Example from the CIMAT Server"
-# get the ip direction of the local computer
 ip="10.10.100.235"
-echo "USERNAME: $USERNAME"
-echo "IP: $ip"
-echo "The ip direction of the local computer is: $ip"
-scp -r ${USERNAME}@$ip:/home/viking/Storage/DanielVallejo/ThesisRealData/MSE_703_RelaxoAI-02 ./
+
+function call()
+{
+    echo "Downloading Example from the CIMAT Server"
+# get the ip direction of the local computer
+    echo "USERNAME: $USERNAME"
+    echo "IP: $ip"
+    echo "The ip direction of the local computer is: $ip"
+    scp -r ${USERNAME}@$ip:/home/viking/Storage/DanielVallejo/ThesisRealData/MSE_703_RelaxoAI-02 $1
+}
+
+call $1
